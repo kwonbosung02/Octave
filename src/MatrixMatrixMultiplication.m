50f504c509be0de09d703d6f7fa786fcfcb1f88c
bskwon0526 <bskwon0526@gmail.com>
2019-07-21 01:16:52 +0900

    commit

---
+++ src/MatrixMatrixMultiplication.m
@@ -0,0 +1,5 @@
+MatrixA = [1,2;3,4;5,6]
+
+MatrixB = [1,2;2,3]
+
+MatrixA * MatrixB
---
+++ src/MatrixMultiplicationIProperties.m
@@ -0,0 +1,10 @@
+MatrixA = [1,2;,3,4]
+MatrixB = [1,1;,0,2]
+
+I = eye(2)
+
+IA = I*MatrixA
+AI = MatrixA*I
+
+MatrixA*MatrixB
+MatrixB*MatrixA
