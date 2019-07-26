d0092be1600a121c961b06339028fc7ce420ba84
bskwon0526 <bskwon0526@gmail.com>
2019-07-26 16:06:45 +0900

    commit

--- src/MovingData.m
+++ src/MovingData.m
@@ -6,7 +6,35 @@ size(A,2)
 v = [1 2 3 4]
 length(v)
 
-#data load -> load
+clear v
+clear A
+clear ans
 
-who
+#data load -> load
+load('featuresX.dat')
+load('featuresY.dat')
+priceY = featuresY
+whos
+
+v = priceY(1:10)
+#save t.txt v -ascii
+
+clear
+
+A = [1,2;3,4;,5,6]
+A(3,2)
+A(2,:)
+A([1 3], :)
+A(:,2) = [10;11;12]
+A
+A = [A,[100;101;102]]
+size(A)
+A(:)
+
+clear
+
+A = [1 2; 3 4; 5 6]
+B = [11,12;13,14;,15,16]
+C = [A B]
+D = [A;B]
 
